5b646ae504179bee1bdcc86a24e36e95605d1cb7
Stefan Helfrich <s.helfrich@fz-juelich.de>
2015-07-23 09:03:27 +0200

    Added documentation for GraphicsExporter

--- src/main/java/vizardous/delegate/impl/graphics/export/GraphicsExporter.java
+++ src/main/java/vizardous/delegate/impl/graphics/export/GraphicsExporter.java
@@ -21,9 +21,10 @@ import vizardous.delegate.impl.fileFilter.SVGFileFilter;
 import vizardous.delegate.impl.graphics.AbstractChart2D;
 
 /**
- * Export functionality for charts.
+ * Central entry point for export functionality of charts and lineage trees.
  * 
- * Methods are called via menu entries for exporting charts.
+ * Methods are called via menu entries for exporting charts and the context menu
+ * when exporting a lineage tree.
  * 
  * @author Charaf E. Azzouzi <c.azzouzi@fz-juelich.de>
  * @author Stefan Helfrich <s.helfrich@fz-juelich.de>
@@ -33,18 +34,20 @@ public class GraphicsExporter {
 	/** The {@link Logger} for this class. */
     final static Logger logger = LoggerFactory.getLogger(GraphicsExporter.class);
 	
-    /** TODO Documentation */
+    /** A map that establishes the connection between {@link FileFilter} descriptions and {@link ChartExporter}s. */
     final static Map<String, ChartExporter> chartExporters = new HashMap<String, ChartExporter>();
     
-    /** TODO Documentation */
+    /** A map that establishes the connection between {@link FileFilter} descriptions and {@link LineageExporter}s. */
     final static Map<String, LineageExporter> lineageExporters = new HashMap<String, LineageExporter>();
     
     static {
+    	/* Initialize the available ChartExporters */
     	chartExporters.put("Portable Network Graphics (*.png)", new PngExporter());
     	chartExporters.put("Scalable Vector Graphics (*.svg)", new SvgExporter());
     	chartExporters.put("Joint Photographic Experts Group Format (*.jpeg)", new JpegExporter());
     	chartExporters.put("Portable Document Format (*.pdf)", new PdfExporter());
     	
+    	/* Initialize the available LineageExporters */
     	lineageExporters.put("Portable Network Graphics (*.png)", new PngExporter());
     	lineageExporters.put("Scalable Vector Graphics (*.svg)", new SvgExporter());
     	lineageExporters.put("Joint Photographic Experts Group Format (*.jpeg)", new JpegExporter());
@@ -52,10 +55,13 @@ public class GraphicsExporter {
     }
     
 	/**
-	 * TODO Documentation
+	 * Exports an {@link AbstractChart2D} to a file. To that end, a file chooser
+	 * is shown, that determines the location of the exported image.
 	 * 
 	 * @param chart
+	 *            The {@link AbstractChart2D} that is exported.
 	 * @param chartArt
+	 *            The type of chart that is exported.
 	 */
 	public static void exportChart2D(AbstractChart2D chart, String chartArt) {
 		JFileChooser myChooser = new JFileChooser();
@@ -87,14 +93,27 @@ public class GraphicsExporter {
 					}
 					
 					exporter.exportChart(chart, filePath);
-				} // if-SelectFile
-			} // iF-ApproveOpt.
+				}
+			}
 		}
 	}
 
 	/**
+	 * Exports a lineage tree in form of an {@link mxGraphComponent} to a file.
+	 * To that end, a file chooser is shown, that determines the location of the
+	 * exported image.
 	 * 
-	 * @param treePanel
+	 * It is coupled with the lineage tree visualization so that either the
+	 * complete tree or a clipped version (as seen on the screen) is exported.
+	 * Furthermore, the current zoom level of the visualization is used for the
+	 * export.
+	 * 
+	 * @param graphComponent
+	 *            The {@link mxGraphComponent} of the lineage tree that is
+	 *            exported.
+	 * @param clipping
+	 *            An option to select wether the complete tree or a clipped
+	 *            version is exported.
 	 */
 	public static void exportLineageTree(mxGraphComponent graphComponent, Clipping clipping) {
 		JFileChooser chooserTreeExp = new JFileChooser();
@@ -126,7 +145,7 @@ public class GraphicsExporter {
 				}
 				
 				exporter.exportLineage(graphComponent, clipping, filePath);				
-			} // if-SelectFile
-		} // iF-ApproveOpt.
+			}
+		}
 	}
 }
